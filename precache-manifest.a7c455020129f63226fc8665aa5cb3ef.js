ed01bf12ed01c9062cca2b2bb25a490c75d5340e
Oleksandr Kryvenets <kralvi987@gmail.com>
2020-04-26 15:37:18 +0300

    Updates

---
+++ precache-manifest.a7c455020129f63226fc8665aa5cb3ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00d39a41f362cb2180c27f4dc84fa650",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "0bb53dc592e7e6725333",
+    "url": "/goit-react-hw-02-feedback/static/js/2.ab12bbff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.ab12bbff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ba64d8fa035867cccf6",
+    "url": "/goit-react-hw-02-feedback/static/js/main.2810a2ce.chunk.js"
+  },
+  {
+    "revision": "f59aa7dbc0213e80a103",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb39d68.js"
+  }
+]);
